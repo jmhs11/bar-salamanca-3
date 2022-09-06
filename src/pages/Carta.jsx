ebf3ecd03ccd0fa8ddb15e9ec9f99cd25509b74e
jmhs11 <josemariahs@gmail.com>
2022-09-06 09:42:48 +0200

    Add data

--- src/pages/Carta.jsx
+++ src/pages/Carta.jsx
@@ -1,4 +1,24 @@
+import carta from '../data/items.json';
+
 const Carta = () => {
-	return <div>Carta</div>;
+	const categories = [...new Set(carta.map(item => item.category))];
+	return (
+		<>
+			{categories.map(item => (
+				<div key={item}>
+					<p>{item}</p>
+					{carta.map(
+						dish =>
+							dish.category === item && (
+								<div className='ml-4' key={dish.id}>
+									{dish.name}
+								</div>
+							)
+					)}
+				</div>
+			))}
+		</>
+	);
 };
+
 export default Carta;
